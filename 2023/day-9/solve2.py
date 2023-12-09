2232e5dc2c9aee2cd0c6906781098429cf6aac05
Miguel Liezun <miguel.liezun@gencove.com>
2023-12-09 12:23:17 -0300

    Solution for day 9

---
+++ 2023/day-9/solve2.py
@@ -0,0 +1,27 @@
+history = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+
+history = open("input.txt", "r").read()
+
+history = [list(map(int, values.split())) for values in history.splitlines()]
+
+def diff(values):
+    diffs = []
+    for i in range(len(values)-1):
+        diffs.append(values[i+1]-values[i])
+    return diffs
+
+
+results = []
+for values in history:
+    changes = [values]
+    while any(changes[-1]):
+        changes.append(diff(changes[-1]))
+    print(changes)
+    last_number = 0
+    for change in reversed(changes):
+        last_number = change[0]-last_number
+    results.append(last_number)
+    
+print("results:", results, sum(results))
