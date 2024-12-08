d962561f53027693ea37335936ccd5d3361d2eec
Miguel Liezun <miguel.liezun@gencove.com>
2024-12-08 10:27:33 +0100

    AOC 2024 day 8

---
+++ 2024/day-8/solve.py
@@ -0,0 +1,58 @@
+from collections import defaultdict
+from typing import Optional
+
+
+antenna_map = """............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
+"""
+
+antenna_map = open("input.txt", "r").read().strip()
+
+antenna_map = [list(l.strip()) for l in antenna_map.splitlines() if l.strip()]
+
+antenna_group = defaultdict(list)
+for i in range(len(antenna_map)):
+    for j in range(len(antenna_map[i])):
+        if antenna_map[i][j] != '.':
+            antenna_group[antenna_map[i][j]].append((i, j))
+            
+def within_range(x: int, y: int, max_x: int, max_y: int):
+    return (0 <= x < max_x) and (0 <= y < max_y)
+
+def find_antinodes(positions: list[tuple[int, int]], max_x: int, max_y: int):
+    antinodes = []
+    for i, p1 in enumerate(positions[:-1]):
+        for p2 in positions[i+1:]:
+            x1, y1 = p1
+            x2, y2 = p2
+            assert x1 <= x2
+            dx, dy = abs(x1-x2), abs(y1-y2)
+            if y1 < y2:
+                a1_x, a1_y = x1-dx, y1-dy
+                a2_x, a2_y = x2+dx, y2+dy
+            else:
+                a1_x, a1_y = x1-dx, y1+dy
+                a2_x, a2_y = x2+dx, y2-dy
+            if within_range(a1_x, a1_y, max_x, max_y):
+                antinodes.append((a1_x, a1_y))
+            if within_range(a2_x, a2_y, max_x, max_y):
+                antinodes.append((a2_x, a2_y))
+    return antinodes
+
+
+result = set()
+for g, pos in antenna_group.items():
+    antis = find_antinodes(pos, len(antenna_map), len(antenna_map[1]))
+    result.update(antis)
+
+print(len(result))
