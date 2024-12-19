0745b8216c2b1c065c3a92f407b26b902fe2f934
Miguel Liezun <miguel.liezun@gencove.com>
2024-12-19 21:23:17 +0100

    Advent of code day 19

---
+++ 2024/day-19/solve.py
@@ -0,0 +1,43 @@
+from collections import defaultdict
+from typing import Optional
+
+
+towels = """r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb"""
+
+towels = open("input.txt", "r").read().strip()
+
+towel_patterns, desired_designs = [
+    l.split(", ") if i == 0 else l.splitlines()
+    for i, l in enumerate(towels.split("\n\n"))
+    if l.strip()
+]
+
+
+def can_be_composed(target: str, substrings: list[str]) -> bool:
+    if not target:
+        return True
+    dp = [False] * (len(target) + 1)
+    dp[0] = True
+    for i in range(1, len(target) + 1):
+        for substring in substrings:
+            if i >= len(substring) and target[i - len(substring):i] == substring:
+                dp[i] = dp[i] or dp[i - len(substring)]
+                if dp[i]:
+                    break
+    return dp[-1]
+
+result = 0
+for design in desired_designs:
+    if can_be_composed(design, towel_patterns):
+        result += 1
+        
+print("result:", result)
---
+++ 2024/day-19/solve2.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+from typing import Optional
+
+
+towels = """r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb"""
+
+towels = open("input.txt", "r").read().strip()
+
+towel_patterns, desired_designs = [
+    l.split(", ") if i == 0 else l.splitlines()
+    for i, l in enumerate(towels.split("\n\n"))
+    if l.strip()
+]
+
+
+def composed_option_count(target: str, substrings: list[str]) -> int:
+    if not target:
+        return 1
+    dp = [0] * (len(target) + 1)
+    dp[0] = 1
+    for i in range(1, len(target) + 1):
+        for substring in substrings:
+            if i >= len(substring) and target[i - len(substring):i] == substring:
+                dp[i] += dp[i - len(substring)]
+    return dp[-1]
+
+result = 0
+for design in desired_designs:
+    result += composed_option_count(design, towel_patterns)
+        
+print("result:", result)
